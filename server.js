b8c949a0b6349352813de07c2fe8a736dc741386
israelebose <massiveebose@gmail.com>
2025-10-24 23:01:23 +0100

    cors issue fix

--- server.js
+++ server.js
@@ -23,12 +23,17 @@ const PORT = process.env.PORT || 5000;
 // =========================
 // MIDDLEWARE
 // =========================
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "https://frontend-pmlz.onrender.com", // your live site
+    "http://localhost:5173" // local dev
+  ],
+  credentials: true
+}));
+
+app.options("*", cors());
 app.use(cookieParser());
 app.use(express.json());
 
