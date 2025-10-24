4fe1e7017cd6246221cf390abc7c327210f25593
israelebose <massiveebose@gmail.com>
2025-10-24 22:15:17 +0100

    cors issue fix

--- server.js
+++ server.js
@@ -23,28 +23,12 @@ const PORT = process.env.PORT || 5000;
 // =========================
 // MIDDLEWARE
 // =========================
-const allowedOrigins = [
-  process.env.FRONTEND_URL, // your live site
-  "http://localhost:5173",              // local dev
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("CORS not allowed from this origin"));
-      }
-    },
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
-
-// Allow preflight requests for all routes
-app.options("*", cors());
 app.use(cookieParser());
 app.use(express.json());
 
