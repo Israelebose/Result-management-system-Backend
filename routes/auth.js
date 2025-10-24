4e203a9675c53d0253f6ddb7b63bc1e582f3da67
israelebose <massiveebose@gmail.com>
2025-10-24 23:46:50 +0100

    cors issue fix

--- routes/auth.js
+++ routes/auth.js
@@ -230,7 +230,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 3600000,
     });
 
